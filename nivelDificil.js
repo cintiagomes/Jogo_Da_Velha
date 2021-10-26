4e6da38fd961e20ec2a1240b28108d716a08da20
balakelly459 <21192911@CFP127ALUNO.LOCAL>
2021-10-26 07:59:56 -0300

    tempo

--- nivelDificil.js
+++ nivelDificil.js
@@ -1,10 +1,10 @@
-'use script'
+'use script';
 
-const blocos = document.querySelectorAll(".bloco");
+const blocos = document.querySelectorAll('.bloco');
 let fimDeJogo = false;
 // let checarTurno = true;
-const PlayerX = "X";
-const PlayerO = "O";
+const PlayerX = 'X';
+const PlayerO = 'O';
 
 const combinacoa = [
     [0, 1, 2],
@@ -14,11 +14,11 @@ const combinacoa = [
     [2, 4, 6],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8]
-]
+    [2, 5, 8],
+];
 
-document.addEventListener("click", (event) =>{
-    if (event.target.matches(".bloco")) {
+document.addEventListener('click', (event) => {
+    if (event.target.matches('.bloco')) {
         //console.log(event.target.id);
         jogada(event.target.id, PlayerX);
         setTimeout(() => bot(), 100);
@@ -28,26 +28,18 @@ document.addEventListener("click", (event) =>{
 function bot() {
     const posicoesDisponiveis = [];
     for (index in blocos) {
-        if (!isNaN(index)){ 
-            if(
-                !blocos[index].classList.contains("x") &&
-                !blocos[index].classList.contains("o")
-            ) {
-            posicoesDisponiveis.push(index);
-            
+        if (!isNaN(index)) {
+            if (!blocos[index].classList.contains('x') && !blocos[index].classList.contains('o')) {
+                posicoesDisponiveis.push(index);
             }
         }
-
     }
 
-    const posicaoAleatoria = Math.floor(
-        Math.random() * posicoesDisponiveis.length
-    );
+    const posicaoAleatoria = Math.floor(Math.random() * posicoesDisponiveis.length);
 
     if (!fimDeJogo) {
-        jogada(posicoesDisponiveis[posicaoAleatoria], PlayerO);   
+        jogada(posicoesDisponiveis[posicaoAleatoria], PlayerO);
     }
-    
 }
 
 function jogada(id, turno) {
@@ -58,15 +50,15 @@ function jogada(id, turno) {
 }
 
 function checarVencendor(turno) {
-    const vencedor = combinacoa.some((comb) =>{
+    const vencedor = combinacoa.some((comb) => {
         return comb.every((index) => {
             return blocos[index].classList.contains(turno);
-        })
+        });
     });
 
     if (vencedor) {
         encerrarJogo(turno);
-    }else if(checarEmpate()){
+    } else if (checarEmpate()) {
         encerrarJogo();
     }
 }
@@ -75,10 +67,8 @@ function checarEmpate() {
     let x = 0;
     let o = 0;
 
-    for (index in blocos){
+    for (index in blocos) {
         if (!isNaN(index)) {
-            
-        
             if (blocos[index].classList.contains(PlayerX)) {
                 x++;
             }
@@ -86,36 +76,33 @@ function checarEmpate() {
             if (blocos[index].classList.contains(PlayerO)) {
                 o++;
             }
-        }    
+        }
     }
 
     return x + o === 9 ? true : false;
-
 }
 
 function encerrarJogo(vencedor = null) {
-
     fimDeJogo = true;
-    const telaEscura = document.getElementById("tela-escura");
-    const h2 = document.createElement("h2");
-    const h3 = document.createElement("h3");
+    const telaEscura = document.getElementById('tela-escura');
+    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
     let mensagem = null;
 
-    telaEscura.style.display = "block";
+    telaEscura.style.display = 'block';
     telaEscura.append(h2);
     telaEscura.append(h3);
 
-    if(vencedor){
+    if (vencedor) {
         h2.innerHTML = `Jogador <span>${vencedor}</span> venceu`;
-    }else{
+    } else {
         h2.innerHTML = 'Empate';
     }
 
-    let contador = 5;
+    let contador = 10;
     setInterval(() => {
         h2.innerHTML = `Continue ${contador--}`;
     }, 1000);
 
-    setTimeout(() => location.reload(), 5000);
-
+    setTimeout(() => location.reload(), 10000);
 }
